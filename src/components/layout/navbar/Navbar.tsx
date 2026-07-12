156095f9531375f7e2055c9468f5b2efc485ca40
Abdur-Rahim-bin-Bakkar <webdesignrahim4061@gmail.com>
2026-07-12 16:03:32 +0600

    add dashbord link in small device

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -262,6 +262,15 @@ export default function Navbar() {
                   </>
                 ) : (
                   <>
+                    {
+                      user?.role !== 'user' && <Link
+                        href="/dashboard"
+                        className="inline-flex my-3 w-full items-center gap-2 rounded-xl border border-cyan-500/20 bg-cyan-500/10 px-4 py-2 font-medium text-cyan-400 transition hover:bg-cyan-500/20"
+                      >
+                        <LayoutDashboardIcon size={18} />
+                        Dashboard
+                      </Link>
+                    }
                     <Link
                       href="/profile"
                       className="group flex items-center gap-3 rounded-full border border-white/10 bg-white/5 px-3 py-2 transition hover:border-cyan-400/40 hover:bg-white/10"
